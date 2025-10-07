0f4ee70dcaba6c362a03fdd3ff546f99fb4052b3
Pommythecat <clarahennings1@gmail.com>
2025-10-07 23:39:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const API_URL = "http://localhost:3001/api/items"; // Later, we will change this to your live URL
 
 async function loadItems() {
-  const res = await fetch(API_URL);
-  const items = await res.json();
-  const list = document.getElementById("itemList");
-  list.innerHTML = "";
   items.forEach(item => {
-    const li = document.createElement("li");
-    li.textContent = `${item.name} - ${item.quantity} at ${item.location}`;
-    list.appendChild(li);
+  const li = document.createElement("li");
+  li.innerHTML = `
+    ${item.name} - ${item.quantity} at ${item.location}
+    <br>
+    <svg id="barcode${item.id}"></svg>
+  `;
+  document.getElementById("itemList").appendChild(li);
+  
+  JsBarcode(`#barcode${item.id}`, item.id.toString(), {
+    format: "CODE128",
+    width: 2,
+    height: 40,
+    displayValue: false
   });
-}
+});
+
 
 document.getElementById("itemForm").addEventListener("submit", async (e) => {
   e.preventDefault();
